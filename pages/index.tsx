987175e0bae8b3614a78ec6b73c6fe5b27a3cc8f
arindam <aroy92647@gmail.com>
2022-06-01 21:28:10 -0700

    add graphcms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,52 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
+
+import {gql, GraphQLClient} from "graphql-request";
 import Hero from '../components/Hero'
 import Projects from '../components/Projects'
 import Services from '../components/Services'
 import Skills from '../components/Skills'
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen w-full flex-col  bg-slate-50 ">
-      <Head>
-        <title>Arindam's Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// @ts-ignore
+const Home: NextPage = ({Projects}) => {
+    console.log(Projects)
+    return (
+        <div className="flex min-h-screen w-full flex-col  bg-slate-50 ">
+            <Head>
+                <title>Arindam's Portfolio</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main className="mx-auto w-full max-w-[1280px] py-6">
-        <Hero />
-        <Projects />
-        <Services />
-        <Skills />
-      </main>
-    </div>
-  )
+            <main className="mx-auto w-full max-w-[1280px] py-6">
+                <Hero/>
+                <Projects/>
+                <Services/>
+                <Skills/>
+            </main>
+        </div>
+    )
+}
+
+export const getStaticProps = async () => {
+    const graphCms = new GraphQLClient(
+        'https://api-ap-south-1.graphcms.com/v2/cl3whaa4egnvk01xi03ir29it/master'
+    )
+    const Projects = await graphCms.request(
+        gql`
+         {
+              projects {
+                id
+                title
+                description
+              }
+            }
+        `
+    )
+    return {
+        props: {
+            Projects
+        }
+    }
 }
 
 export default Home
