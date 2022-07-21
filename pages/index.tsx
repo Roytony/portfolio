6a8ebce13dcf0be1c02b29af0e5846e10c87a2f3
Arindam Roy <73665685+arindam324@users.noreply.github.com>
2022-07-21 03:22:20 -0700

    remove some comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Suspense, lazy } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-// import { NextSeo } from 'next-seo'
 
 import Hero from '../components/Hero'
 import Services from '../components/Services'
@@ -45,39 +44,6 @@ const Home: NextPage<{ data: any }> = ({ data }) => {
         <title>Arindam's Portfolio</title>
         <link rel="icon" href="/pic3.ico" />
       </Head>
-      {/* <NextSeo
-        title="Arindam's Roy Portfolio"
-        description="Arindam Roy is a software engineer and a full-stack developer. He is a self-taught developer and has a passion for learning new technologies and building cool stuff."
-        canonical="https://www.canonical.ie/"
-        twitter={{
-          handle: '@Arindam20103910',
-          site: 'portfolio-chi-eight-57.vercel.app',
-          cardType: 'summary_large_image',
-        }}
-        openGraph={{
-          url: 'https://portfolio-chi-eight-57.vercel.app/',
-          title: "Arindam's Portfolio",
-          description: 'This is a simple portfolio of Arindam Roy ',
-          images: [
-            {
-              url: '/pic3.png',
-              width: 800,
-              height: 600,
-              alt: 'Arindam Roy',
-              type: 'image/jpeg',
-            },
-            {
-              url: 'https://media.graphassets.com/UdEP7CwOSN6zRrpIxgLe',
-              width: 900,
-              height: 800,
-              alt: 'Sass Landing Page',
-              type: 'image/jpeg',
-            },
-          ],
-          site_name: "Arindam's portfolio",
-        }}
-      /> */}
-
       <main className="mx-auto w-full max-w-[1280px] py-6">
         <Hero />
         <Suspense fallback={<div>Loading...</div>}>
